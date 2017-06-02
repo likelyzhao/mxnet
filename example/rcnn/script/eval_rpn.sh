3086312ea0a593c8d7edc6747e546dc04040368f
root <likely_zhao@163.com>
2017-06-02 03:58:05 +0000

    add eval rpn script

---
+++ example/rcnn/script/eval_rpn.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# 网络模型：resnet训练的第2轮epoch的结果
+# 数据集：ILSVRC 2017 分类的val数据集
+LOG=eval_proposal_imagenet_baseline.log
+
+rm -rf ${LOG}
+
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
+export PYTHONUNBUFFERED=1
+
+nohup python -m rcnn.tools.test_rpn --network resnet152                        \
+                                  --dataset imagenet           \
+                                  --image_set train                           \
+                                  --prefix model/e2e          \
+                                  --gpu 2                                   \
+                                  --epoch 7                                \
+                                  >${LOG} 2>&1 &
